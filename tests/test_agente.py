2a2985a2c89aab0e58a8b2a4bbf6300fd8ada8dc
AshleyAlfaroAguilar <alfaroashley25@gmail.com>
2026-02-12 00:22:52 -0600

    Add unit tests

---
+++ tests/test_agente.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestImports(unittest.TestCase):
+    def test_import_agente(self):
+        import agente_financiero  
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
